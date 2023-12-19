047defa252dd17c4830ae6cdfe8911144be59cbd
Mohamed Abdelaal <mabdelaal86@gmail.com>
2023-12-19 09:37:55 +0100

    day 18 - part 2 trial

--- aoc23/lib/day18.dart
+++ aoc23/lib/day18.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:basics/int_basics.dart';
+
 import 'common.dart';
 
 final example = r"""
@@ -21,47 +23,65 @@ U 2 (#7a21e3)
 
 late final List<String> data;
 
-final dataRe = RegExp(r"^(\w) (\d+) \(#([0-9a-f]+)\)$");
 final str2Dir = {"U": Dir.n, "D": Dir.s, "R": Dir.e, "L": Dir.w};
+final num2Dir = {"3": Dir.n, "1": Dir.s, "0": Dir.e, "2": Dir.w};
 
 Future<void> main() async {
-  data = await getData().toList();
-  // data = example.split("\n");
+  // data = await getData().toList();
+  data = example.split("\n");
 
   // data.printAll();
+  final processed1 = data.map(process1);
+  List<Point<int>> edges1 = getEdges(processed1);
 
-  final edges = [Point(0, 0)];
-  data.map(process).forEach((e) => fillEdges(edges, e));
-  adjustEdges(edges);
+  printEdges(edges1);
 
-  // printEdges(edges);
-
-  final part1 = area(edges);
+  final part1 = area(edges1);
   print(part1);
+
+  final processed2 = data.map(process2);
+  List<Point<int>> edges2 = getEdges(processed2);
+
+  print(processed2);
+  printEdges(edges2);
+}
+
+List<Point<int>> getEdges(Iterable<(Dir, int)> processed) {
+  final edges = [Point(0, 0)];
+  for (var e in processed) {
+    fillEdges(edges, e);
+  }
+  adjustEdges(edges);
+  return edges;
 }
 
 void printEdges(List<Point<int>> edges) {
   final m = edges.reduce((val, elm) => Point(max(val.x, elm.x), max(val.y, elm.y)));
-  for (var r in Iterable<int>.generate(m.y + 1)) {
-    print(Iterable.generate(m.x + 1, (c) => edges.contains(Point(c, r)) ? "#" : ".").join());
+  for (final r in min(1000, m.y + 1).range) {
+    print(min(1000, m.x + 1).range.map((c) => edges.contains(Point(c, r)) ? "#" : ".").join());
   }
 }
 
-(Dir, int, String) process(String line) {
-  final m = dataRe.firstMatch(line)!;
-  return (str2Dir[m[1]]!, int.parse(m[2]!), m[3]!);
+(Dir, int) process1(String line) {
+  final parts = line.split(" ");
+  return (str2Dir[parts[0]]!, int.parse(parts[1]));
 }
 
-void fillEdges(List<Point<int>> edges, (Dir, int, String) item) {
+(Dir, int) process2(String line) {
+  final color = line.split(" ").last;
+  return (num2Dir[color[7]]!, int.parse(color.substring(2, 7), radix: 16));
+}
+
+void fillEdges(List<Point<int>> edges, (Dir, int) item) {
   final delta = dirDelta[item.$1]!;
   final last = edges.last;
-  final newEdges = Iterable.generate(item.$2, (i) => delta * (i + 1) + last);
+  final newEdges = item.$2.range.map((i) => delta * (i + 1) + last);
   edges.addAll(newEdges);
 }
 
 void adjustEdges(List<Point<int>> edges) {
   final topLeft = edges.reduce((val, elm) => Point(min(val.x, elm.x), min(val.y, elm.y)));
-  for (var (index, point) in edges.indexed) {
+  for (final (index, point) in edges.indexed) {
     edges[index] = point - topLeft;
   }
 }
