9d7e26bb2282a29049de1ba6ccbd8aef01f35244
Mohamed Abdelaal <mabdelaa@volvocars.com>
2024-12-02 09:56:34 +0100

    little refactor

--- aoc24/src/day02b.rs
+++ aoc24/src/day02b.rs
@@ -14,35 +14,40 @@ fn process(lines: impl IntoIterator<Item=String>) -> i32 {
             .map(|n| n.parse().unwrap())
             .collect_vec();
 
-        let safe = is_safe(&values);
-        if safe == (0, 0) {
+        if  is_tolerated_safe(&values) {
             safe_count += 1;
-            continue;
-        }
-
-        for i in 0..values.len() {
-            let mut v2 = values.clone();
-            v2.remove(i);
-            if is_safe(&v2) == (0, 0) {
-                safe_count += 1;
-                break;
-            }
         }
     }
 
     safe_count
 }
 
-fn is_safe(values: &Vec<i32>) -> (usize, usize) {
+fn is_tolerated_safe(values: &Vec<i32>) -> bool {
+    if  is_safe(&values) {
+        return true;
+    }
+
+    for i in 0..values.len() {
+        let mut v2 = values.clone();
+        v2.remove(i);
+        if  is_safe(&v2) {
+            return true
+        }
+    }
+
+    false
+}
+
+fn is_safe(values: &Vec<i32>) -> bool {
     let d1 = values[1] - values[0];
     for i in 1..values.len() {
         let d = values[i] - values[i - 1];
         if d.abs() < 1 || d.abs() > 3 || d.is_positive() != d1.is_positive() {
-            return (i - 1, i);
+            return false;
         }
     }
 
-    (0, 0)
+    true
 }
 
 #[cfg(test)]
