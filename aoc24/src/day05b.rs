d7c431bbd6404a86baa6bd3ce4981e544163cb06
Mohamed Abdelaal <mabdelaa@volvocars.com>
2024-12-06 12:19:25 +0100

    AoC 24 - day 05 - b

--- aoc24/src/day05b.rs
+++ aoc24/src/day05b.rs
@@ -9,7 +9,7 @@ pub fn main() -> u32 {
 
 fn process(lines: impl Iterator<Item=String>) -> u32 {
     let mut read_rules = true;
-    // page : pages after
+    // page : pages before
     let mut rules: PageRule = HashMap::new();
     let mut res = 0;
 
@@ -35,22 +35,55 @@ fn process_rule(rules: &mut PageRule, line: &str) {
 
     if !rules.contains_key(&p1) { rules.insert(p1, Vec::new()); }
     if !rules.contains_key(&p2) { rules.insert(p2, Vec::new()); }
-    rules.get_mut(&p1).unwrap().push(p2);
+    rules.get_mut(&p2).unwrap().push(p1);
 }
 
 fn process_update(rules: &mut PageRule, line: &str) -> u32 {
-    let pages = line.split(",")
-        .map(|p| p.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+    let mut pages = parse_update(line);
 
+    if is_valid_update(rules, &pages) {
+        return 0;
+    }
+
+    rearrange_pages(rules, &mut pages);
+
+    pages[pages.len() / 2]
+}
+
+fn parse_update(line: &str) -> Vec<u32> {
+    line.split(",")
+        .map(|p| p.parse::<u32>().unwrap())
+        .collect()
+}
+
+fn is_valid_update(rules: &PageRule, pages: &[u32]) -> bool {
     for i in 0..pages.len()-1 {
         let page_rule = rules.get(&pages[i]).unwrap();
-        if pages[i+1..].iter().any(|&p| !page_rule.contains(&p)) {
-            return 0;
+        if pages[i+1..].iter().any(|&p| page_rule.contains(&p)) {
+            return false;
         }
     }
 
-    pages[pages.len() / 2]
+    true
+}
+
+fn rearrange_pages(rules: &PageRule, pages: &mut Vec<u32>) {
+    let mut i: usize = 0;
+    while i < pages.len()-1 {
+        let pages_before = &rules.get(&pages[i]).unwrap();
+        let wrong_pages = pages[i+1..].iter()
+            .filter(|&p| pages_before.contains(p))
+            .copied()
+            .collect::<Vec<u32>>();
+
+        if wrong_pages.is_empty() {
+            i += 1;
+            continue;
+        }
+
+        pages.retain(|&p| !wrong_pages.contains(&p));
+        wrong_pages.iter().for_each(|&p| pages.insert(i, p));
+    }
 }
 
 #[cfg(test)]
@@ -90,6 +123,7 @@ mod tests {
             97,13,75,29,47
         "}.lines().map(|l| l.to_string());
 
-        assert_eq!(process(lines), 143);
+        // assert_eq!(process(lines), 47+13+75);
+        assert_eq!(process(lines), 123);
     }
 }
--- aoc24/src/main.rs
+++ aoc24/src/main.rs
@@ -4,7 +4,7 @@ mod day01a; mod day01b;
 mod day02a; mod day02b;
 mod day03a; mod day03b;
 mod day04a; mod day04b;
-mod day05a;
+mod day05a; mod day05b;
 
 fn main() {
     let puzzle = std::env::args().nth(1).expect("no puzzle given");
@@ -20,6 +20,7 @@ fn main() {
         "day04a" => day04a::main(),
         "day04b" => day04b::main(),
         "day05a" => day05a::main(),
+        "day05b" => day05b::main(),
         _ => panic!("Invalid puzzle name"),
     };
 
